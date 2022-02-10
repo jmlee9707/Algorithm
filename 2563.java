2605ef63e40b9a4ad3d424697e61848d65e9171e
jjenny7 <72871348+jmlee9707@users.noreply.github.com>
2022-02-10 23:52:58 +0900

    색종이

---
+++ 2563.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main_2563 {
+	static int N;
+	static int [][] paper;
+	static int size;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		
+		size = 0; // 검은 종이 사이즈 
+
+		paper = new int[100][100];
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				paper[i][j]=0;
+			}
+		}
+		
+		for(int t = 0; t<N; t++) {
+			
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			
+			for(int i=x; i<x+10; i++) {
+				for(int j=y; j<y+10; j++) {
+					if(paper[i][j]!=1) {
+						paper[i][j] =1;
+						size++;
+					}
+					else {
+						continue;
+					}
+				}
+			}
+		}
+	
+		System.out.println(size);
+		
+	}
+
+}
