173adcfd55c6e81c0e2c73bf0141b1c06cc747d1
jjenny7 <72871348+jmlee9707@users.noreply.github.com>
2022-02-17 23:15:51 +0900

    동전0

---
+++ Main_11047.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+public class Main_11047 {
+    static int N, K, count;
+    static int[] coin;
+    public static void main(String[] args) {
+       // TODO Auto-generated method stub
+		Scanner sc  = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        coin = new int [N];
+        
+        
+        int money = K;
+        
+        for(int i=0; i<N; i++) {
+    	   coin[i] = sc.nextInt();
+       }
+
+        int i = N-1;
+        int res = 0;
+        count = 0;
+        
+        while(i>=0){
+        	if(money==0) break;
+        	
+            // 돈이 가지고 있는 거스름돈 수보다 작고 나누어 떨어지면
+            if(money<coin[i]) {
+            	i--;
+            	continue;
+            }
+
+            else if (money>=coin[i] && money/coin[i]>0){
+                count = money/coin[i];
+                money = money-(coin[i]*count);
+                res +=count;
+                i--;
+            }
+        }
+
+        System.out.println(res);
+    }
+
+
+}
+
