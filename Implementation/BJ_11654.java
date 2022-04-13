7b49812d07bb8637f64a836e9ef378233bfe9d6a
이정민 <jmlee9707@naver.com>
2022-04-13 10:12:46 +0900

    아스키코드

---
+++ Implementation/BJ_11654.java
@@ -0,0 +1,15 @@
+package Implementation;
+
+import java.util.Scanner;
+
+public class BJ_11654 {
+    public static void main(String[] args) {
+        // 아스키코드
+        Scanner sc = new Scanner(System.in);
+        char s = sc.nextLine().charAt(0);
+
+
+        System.out.println((int)s);
+
+    }
+}
