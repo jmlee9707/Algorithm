a31cb38e11a9fd89bbc6ae6ae275bb4e030ad307
이정민 <jmlee9707@naver.com>
2022-04-17 23:59:49 +0900

    제리와 톰

---
+++ Math/BJ_16430.java
@@ -0,0 +1,17 @@
+package Math;
+
+import java.util.Scanner;
+
+//  제리와 톰
+public class BJ_16430 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+    
+        a = b-a;
+    
+        System.out.println(a+" "+b);
+    }
+}
