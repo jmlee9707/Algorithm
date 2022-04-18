c842f4b1a739be57a497682b433443847610c906
이정민 <jmlee9707@naver.com>
2022-04-18 13:50:18 +0900

    카드2

---
+++ Data_Structure/BJ_2164.java
@@ -0,0 +1,29 @@
+package Data_Structure;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BJ_2164 {
+    // 카드2
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Queue <Integer> que = new LinkedList<>();
+        for(int i=1; i<=N; i++){
+            que.add(i); //
+        }
+
+        int cnt = N;
+        while(que.size()>1){
+            que.poll(); // 제일 위의 카드 버리기
+            int tmp = que.peek();
+            System.out.println(tmp);
+            que.poll();
+            que.add(tmp);
+        }
+
+        System.out.println(que.peek());
+
+    }
+}
