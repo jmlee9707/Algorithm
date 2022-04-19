70b9f4e3a5b2f9682ace65230000533470c48dd8
jjenny7 <jmlee9707@naver.com>
2022-04-19 22:19:52 +0900

    ??!

---
+++ String/BJ_10926.java
@@ -0,0 +1,12 @@
+package String;
+
+import java.util.Scanner;
+
+public class BJ_10926 {
+    // ??!
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s+"??!");
+    }
+}
