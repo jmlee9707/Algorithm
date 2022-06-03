29c50ae33a76646ea41fd41ff79462706d5d753b
jmlee9707 <jmlee9707@naver.com>
2022-06-04 02:16:31 +0900

    분해합

--- ExhaustiveSearch/BJ_2231.java
+++ ExhaustiveSearch/BJ_2231.java
@@ -1,4 +1,4 @@
-package BP;
+package ExhaustiveSearch;
 
 import java.util.Scanner;
 
@@ -8,15 +8,15 @@ public class BJ_2231 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int res = 0;
-        for(int i=1; i<N; i++){
+        for (int i = 1; i < N; i++) {
             int num = i;
             int sum = i;
-            while(num>=1){
-                sum += num%10;
-                num = num/10;
+            while (num >= 1) {
+                sum += num % 10;
+                num = num / 10;
             }
 
-            if(sum==N){
+            if (sum == N) {
                 res = i;
                 break;
             }
