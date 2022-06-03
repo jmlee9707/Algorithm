d1f5d00f6162bf27acc34e33b9eb9d8e343f369c
jmlee9707 <jmlee9707@naver.com>
2022-06-03 18:29:08 +0900

    제리와 톰

--- Math/BJ_16430.java
+++ Math/BJ_16430.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 //  제리와 톰
 public class BJ_16430 {
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-    
-        a = b-a;
-    
-        System.out.println(a+" "+b);
+
+        a = b - a;
+
+        System.out.println(a + " " + b);
     }
 }
