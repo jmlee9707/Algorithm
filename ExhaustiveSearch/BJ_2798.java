1663c17646e842e8787f869f52a1d08086b1dfef
jmlee9707 <jmlee9707@naver.com>
2022-06-04 02:16:44 +0900

    블랙잭

--- ExhaustiveSearch/BJ_2798.java
+++ ExhaustiveSearch/BJ_2798.java
@@ -1,4 +1,4 @@
-package BP;
+package ExhaustiveSearch;
 
 import java.util.Scanner;
 
@@ -11,18 +11,18 @@ public class BJ_2798 {
         int M = sc.nextInt();
 
         int[] card = new int[N];
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             card[i] = sc.nextInt();
         }
         // 순열 조합 사용
         // nC3 -> m과 가장 가까운 수
         int max = 0;
 
-        for(int i=0; i<N; i++){
-            for(int j=i+1; j<N;j++){
-                for(int k=j+1; k<N; k++){
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
                     int sum = card[i] + card[j] + card[k];
-                    if(sum<=M && sum>max){
+                    if (sum <= M && sum > max) {
                         max = sum;
                     }
                 }
