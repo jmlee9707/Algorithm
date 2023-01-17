f20486a7e2ded9b3a6f5e4df28089bf09d99fe46
jmlee9707 <jmlee9707@naver.com>
2023-01-17 18:02:01 +0900

    프린터 큐

--- Baekjoon/Data_Structure/BJ_1966.java
+++ Baekjoon/Data_Structure/BJ_1966.java
@@ -1,72 +1,60 @@
 package Data_Structure;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class BJ_1966 {
     // 프린터 큐
     static int T, N, M, res;
+    static int[] priority; // 중요도 1~9
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        T = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
+        StringTokenizer st;
+
         // 테스트 케이스 시작
         for (int t = 1; t <= T; t++) {
-            N = sc.nextInt(); // 문서의 개수
-            M = sc.nextInt(); // 궁금한 문서
-            res = 0; // M번째 문서가 몇번째로 출력되는가?
+            st = new StringTokenizer(br.readLine(), " ");
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            res = 0; // M번째 문서가 몇번째로 출력되는가? // 1부터 시작
+            String[] temp = br.readLine().split(" ");
 
             Queue<int[]> que = new LinkedList<>(); // 큐 생성
+            priority = new int[10];
 
             // 중요도 1-9사이 정수
             for (int i = 0; i < N; i++) {
-                que.add(new int[] { i, sc.nextInt() }); // 초기 순서, 중요서
+                int num = Integer.parseInt(temp[i]);
+                priority[num]++; // 중요도 체크
+
+                que.add(new int[] { i, num }); // 초기 순서, 중요도
             }
 
             while (!que.isEmpty()) {
-                int[] now = que.poll(); // 현재 문서
-                int cnt = 0;
-                int impol = now[1]; // 현재 문서의 중요도
-
-            }
-
-            for (int i = 0; i < N; i++) {
-
-                // int impol = arr[i]; // 중요도
-                // int cnt = 0; //
-                for (int j = 0; j < N; j++) {
-                    if (j == i)
-                        continue; // 같은숫자는 패스
-
-                    if (arr[j] > impol) {
-                        // 나머지 문서 중에 현재보다 중요도가 큰 문서가 있다면
-                        que.add(i); // 내가 현재 몇번째 문서인지를 저장
-                        cnt++;
+                int[] doc = que.poll();
+                boolean check = false;
+                // 중요도 높은 문서가 있는가?
+                for (int c = doc[1] + 1; c <= 9; c++) {
+                    if (priority[c] >= 1) {
+                        check = true;
                         break;
                     }
                 }
-                if (cnt == 0) {
-                    // 내가 제일 중요도가 큰 문서라면
-                    printq.add(i); // 몇번째 문서인지 푸시
+
+                if (check) {
+                    que.add(doc); // 뒤에 재배치
+                } else {
+                    priority[doc[1]]--;
+                    res++; // 인쇄
+
+                    if (M == doc[0]) {
+                        break;
+                    }
                 }
             }
-
-            while (!que.isEmpty()) {
-                printq.add(que.poll()); // 하나의 큐로 이동시키기
-            }
-
-            for (int i = 1; i <= N; i++) {
-                System.out.println(printq.peek());
-                if (printq.peek() == M) { // 내가 궁금한 문서의 순서라면
-                    res = i;
-                    break; // 종료
-                }
-                printq.poll(); // 하나씩 값 꺼내서 날리기
-            }
-
             System.out.println(res);
-
         }
 
     }
