c41f1a159d07e5b0903a1f0146f0a3c9a639bba2
jmlee9707 <jmlee9707@naver.com>
2024-10-12 23:50:56 +0900

    241012: 돌게임

---
+++ Baekjoon/Greedy/BJ_9655.java
@@ -0,0 +1,17 @@
+package Greedy;
+
+import java.util.Scanner;
+
+public class BJ_9655 {
+    /*
+     * 백준 9655
+     * 돌게임
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%2==0) System.out.println("CY");
+        else System.out.println("SK");
+
+    }
+}
